7e6e794c378487c7dabd35cb41ae742b18075110
echo 'hello world <ssmyth1071@gmail.com>
2018-02-06 14:00:24 -0500

    add full CRUD functionality

--- blog/quickstart.py
+++ blog/quickstart.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from apiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
+from django.utils import timezone
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 store = file.Storage('storage.json')
@@ -11,15 +12,16 @@ if not creds or creds.invalid:
     creds = tools.run_flow(flow, store)
 GCAL = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
 
-GMT_OFF = '-05:00'      # PDT/MST/GMT-7
+TIMEZONE = 'America/New_York'      # PDT/MST/GMT-7
 EVENT = {
     'summary': 'Dinner with friends',
-    'start':  {'dateTime': '2018-02-10T19:00:00%s' % GMT_OFF},
-    'end':    {'dateTime': '2018-02-10T22:00:00%s' % GMT_OFF},
+    'start':  {'dateTime': '2018-02-10T12:00:00', 'timeZone': TIMEZONE},
+    'end':    {'dateTime': '2018-02-10T22:00:00', 'timeZone': TIMEZONE},
     # 'attendees': [
     #     {'email': 'seanesmyth@yahoo.com'},
     # ],
-    'location': 'Pizzeria Uno'
+    'location': 'Pizzeria Uno',
+    'recurrence': [ 'RRULE:FREQ=WEEKLY;UNTIL=20180224']
 }
 
 e = GCAL.events().insert(calendarId='primary',
@@ -27,5 +29,9 @@ e = GCAL.events().insert(calendarId='primary',
 
 print('''*** %r event added:
     Start: %s
-    End:   %s''' % (e['summary'].encode('utf-8'),
-        e['start']['dateTime'], e['end']['dateTime']))
+    End:   %s
+    location: %s
+    organizer: %s ''' % (e['summary'].encode('utf-8'),
+        e['start']['dateTime'], e['end']['dateTime'], e['location'], e['organizer']))
+# Python 3.6 string interpolation style print
+print(f"My name is {e['summary']}, my age next year is {age+1}.")
